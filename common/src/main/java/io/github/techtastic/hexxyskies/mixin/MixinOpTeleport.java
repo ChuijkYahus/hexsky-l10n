cebbad4d3502e85fc405acd04b866d43e51ffaaf
TechTastic <74630543+TechTastic@users.noreply.github.com>
2026-01-11 02:22:32 -0600

    Missed a spot, started on fixing Greater Teleport

---
+++ common/src/main/java/io/github/techtastic/hexxyskies/mixin/MixinOpTeleport.java
@@ -0,0 +1,8 @@
+package io.github.techtastic.hexxyskies.mixin;
+
+import at.petrak.hexcasting.common.casting.actions.spells.great.OpTeleport;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(OpTeleport.class)
+public class MixinOpTeleport {
+}
